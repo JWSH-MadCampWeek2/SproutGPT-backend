431f516f5349bd3ce1c28b23cc81ce19507ddf0a
sephia16 <sspringson@gmail.com>
2024-01-06 23:30:57 +0900

    finished oauth

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ from dotenv import load_dotenv
 import os
 import requests
 from flask_pymongo import PyMongo
-# from controller import Oauth
-# from model import UserModel, UserData
+from pymongo import MongoClient
 
 CLIENT_ID = os.environ.get("CLIENT_ID")
 CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
 REDIRECT_URI = os.environ.get("REDIRECT_URI")
+CLIENT = os.environ.get("CLIENT")
 
 
 
@@ -19,8 +19,12 @@ REDIRECT_URI = os.environ.get("REDIRECT_URI")
 
 app = Flask(__name__)
 
-app.config["MONGO_URI"] = "mongodb+srv://sspringson:<password>@madcampweek2.kdhm8ha.mongodb.net/"
-mongo = PyMongo(app)
+client = MongoClient("CLIENT")
+db = client['MadCampWeek2']
+collection_User = db['User']
+
+
+
 
 
 @app.route("/oauth", methods=['POST'])
@@ -61,6 +65,23 @@ def oauth_api():
         user_response.raise_for_status()
         user_info = user_response.json()
 
+        existing_user = collection_User.find_one({"user_id": user_info.get('id')})
+        if existing_user:
+            return jsonify({'message': 'User already exists'}), 200
+
+        user_id = user_info.get('id')
+        user_nickname = user_info['kakao_account']['profile']['nickname']
+        user_profile_image = user_info['kakao_account']['profile']['profile_image_url']
+
+        # Prepare the document to insert into MongoDB
+        user_document = {
+            "user_id": user_id,
+            "nickname": user_nickname,
+            "profile_image": user_profile_image
+        }
+
+        # Step 4: Insert User Information into MongoDB
+        result = collection_User.insert_one(user_document)
         # Step 4: Return User Information
         return jsonify(user_info)
     
@@ -69,21 +90,9 @@ def oauth_api():
         return jsonify({'error': str(e)}), 500
     
 
-@app.route('/add', methods=['POST'])
-def add_data():
-    # Assume JSON data comes with the POST request
-    data = request.json
-    if not data:
-        return jsonify({"error": "No data provided"}), 400
-
-    try:
-        # Insert the data into the 'myCollection' collection
-        result = mongo.db.myCollection.insert_one(data)
-        # Return the ID of the inserted document
-        return jsonify({"_id": str(result.inserted_id)}), 201
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True, host = "143.248.219.4", port = 8080)
+
+
 
