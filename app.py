1babf2723b27bda17a324fc84c4c9efe84f13204
sephia16 <sspringson@gmail.com>
2024-01-06 17:36:51 +0900

    hi

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 
 from flask import Flask, render_template, request, jsonify, make_response
+from flask_cors import CORS
 from flask_jwt_extended import (
     JWTManager, create_access_token, 
     get_jwt_identity, jwt_required,
@@ -13,6 +14,7 @@ from model import UserModel, UserData
 
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"*": {"origins": "*"}})
 app.config['JWT_SECRET_KEY'] = "I'M IML."
 app.config['JWT_TOKEN_LOCATION'] = ['cookies']
 app.config['JWT_COOKIE_SECURE'] = False
@@ -22,42 +24,16 @@ app.config['JWT_REFRESH_TOKEN_EXPIRES'] = 100
 jwt = JWTManager(app)
 
 
-@app.route("/")
+@app.route("/",methods=['POST'])
 def index():
+    data = request.get_json()
+    code = data.get('code') 
+    print(code)
     return render_template('index.html')
 
 
 @app.route("/oauth", methods=['POST'])
 def oauth_api():
-    """
-    # OAuth API [GET]
-    사용자로부터 authorization code를 인자로 받은 후,
-    아래의 과정 수행함
-    1. 전달받은 authorization code를 통해서
-        access_token, refresh_token을 발급.
-    2. access_token을 이용해서, Kakao에서 사용자 식별 정보 획득
-    3. 해당 식별 정보를 서비스 DB에 저장 (회원가입)
-    3-1. 만약 이미 있을 경우, (3) 과정 스킵
-    4. 사용자 식별 id를 바탕으로 서비스 전용 access_token 생성
-     """
-    # code = str(request.args.get('code'))
-    
-    # oauth = Oauth()
-    # auth_info = oauth.auth(code)
-    # user = oauth.userinfo("Bearer " + auth_info['access_token'])
-    
-    # user = UserData(user)
-    # UserModel().upsert_user(user)
-
-    # resp = make_response(render_template('index.html'))
-    # access_token = create_access_token(identity=user.id)
-    # refresh_token = create_refresh_token(identity=user.id)
-    # resp.set_cookie("logined", "true")
-    # set_access_cookies(resp, access_token)
-    # set_refresh_cookies(resp, refresh_token)
-
-    #return resp
-
     data = request.get_json()
     code = data.get('code')  # 'code' 키로 인가 코드를 받음
     
@@ -157,4 +133,4 @@ def oauth_userinfo_api():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host = "143.248.219.4", port = 8080)
